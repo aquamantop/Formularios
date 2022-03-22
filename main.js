1ae6890bd6d3a9db43a5aa61e271a4818f091331
Franco Rampazzo <franco.rodrigo.rampazzo@gmail.com>
2022-03-22 19:07:05 -0300

    arreglar prevent

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const btn = document.querySelector(".boton");
-const btn2 = document.querySelector(".boton2");
+const btn2 = document.querySelectorAll(".boton2");
 
 btn.addEventListener("submit", function(e){
     e.preventDefault();
+    alert("Listo!");
+
 });
 
 btn2.forEach(element => {
     element.addEventListener("submit", (e) =>{
         e.preventDefault();
+        alert("Listo!");
     });
 });
